543713039f3807a90303f4d8cb83c9c3f75300ac
Victor Giovannoni <vernalhav@usp.br>
2021-05-30 11:28:15 -0300

    refactor(PhysicsBody): removed unnecessary calculations

--- src/graphics/PhysicsBody.cpp
+++ src/graphics/PhysicsBody.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-
 #include "PhysicsBody.h"
 
 
 void PhysicsBody::update() {
 	kinematics.kinematicsUpdate();
-	std::cout << sceneObject->getName() << ": " << kinematics.linearVelocity << std::endl;
 	sceneObject->transform.rotation += kinematics.angularVelocity * (float)Component::deltaTime;
 
 	float curRotation = sceneObject->transform.rotation;
@@ -18,7 +15,4 @@ void PhysicsBody::update() {
 void KinematicProperties::kinematicsUpdate() {
 	linearVelocity = utils::clamp(linearVelocity - utils::sign(linearVelocity) * linearDrag + linearAcceleration, -terminalLinearVelocity, terminalLinearVelocity);
 	angularVelocity = utils::clamp(angularVelocity - utils::sign(angularVelocity) * angularDrag + angularAcceleration, -terminalAngularVelocity, terminalAngularVelocity);
-
-	if (abs(linearVelocity) <= linearDrag) linearVelocity = 0;
-	if (abs(angularVelocity) <= angularDrag) angularVelocity = 0;
 }
